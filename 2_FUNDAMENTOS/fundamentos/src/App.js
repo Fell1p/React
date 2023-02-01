eb113b6018d24913fd447cc3904909025aa2787e
Fell1p <luofelipe@hotmail.com>
2023-01-31 23:30:31 -0300

    Utilizando eventos

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -2,14 +2,16 @@ import FirstComponent from './components/FirstComponent';
 import './App.css';
 import TemplateExpressions from './components/TempleteExpressions';
 import MyComponent from './components/MyComponent';
+import Events from './components/Events';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> {/*Quando trabalhamos com React alguns comandos com as "Class" podem entrar em conflito com o HTML quando reindenizado no navegador, para evitar substituimos o "class" por "className"*/}
       <h1>Fundamentos React</h1>
       <FirstComponent />
       <TemplateExpressions />
       <MyComponent /> {/* Componente antes reindenizado em "FirtComponent" agora reaproveitado no componente Pai */}
+      <Events />
     </div>
   );
 }
