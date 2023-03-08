34e4f5989e7529f9f293fd99a9171afd82cd9a34
Fell1p <luofelipe@hotmail.com>
2023-03-07 22:48:02 -0300

    utilizando o onChange

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,20 +1,42 @@
 import './MyForm.css';
+import { useState } from 'react';
 
 const MyForm = () => {
+  // 3 - grenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+
+  console.log(name);
+  console.log(email);
+
   return (
     <div>
         {/* 1 - criação de formulario */}
         <form>
             <div>
                 <label htmlFor="name">Name: </label>
-                <input type="text" name="name" placeholder="Digite seu nome" />
+                <input 
+                  type="text" 
+                  name="name" 
+                  placeholder="Digite seu nome" 
+                  onChange={handleName} 
+                />
             </div>
             {/* 2 - label envolvendo input */}
             <label>
-                <span>E-mail</span>
+                <span>E-mail: </span>
                 <input type="email" name="email" />
             </label>
-            <input type="submit" value="Enviar" placeholder="Digite seu email"/>
+            <input 
+              type="submit" 
+              value="Enviar" 
+              placeholder="Digite seu email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
         </form>
     </div>
   )
