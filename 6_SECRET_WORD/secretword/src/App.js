a4ee059590e1e9c29c9eed8930c02e861840c970
Fell1p <luofelipe@hotmail.com>
2023-03-13 23:01:57 -0300

    Projeto SecretWord iniciado

---
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StartSceen from './components/StartScreen'
+
+function App() {
+  return (
+    <div className="App">
+      <StartSceen />
+    </div>
+  );
+}
+
+export default App;
