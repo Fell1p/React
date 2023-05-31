1f92c0ab2360fb29a98469b51f0553b745e93c57
Fell1p <luofelipe@hotmail.com>
2023-05-30 23:38:31 -0300

    useEffect e requisicoes

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [products, setProducts] = useState([])
+
+  // 1 - resgatando dados
+  useEffect(async () => {
+
+    const res = await fetch(url)
+
+    const data = await res.json()
+
+    setProducts(data)
+
+  })
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
