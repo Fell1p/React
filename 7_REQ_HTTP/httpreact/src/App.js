70749d4e2d3fff8e2ecc357d2190259a8a71c0e8
Fell1p <luofelipe@hotmail.com>
2023-05-31 23:40:38 -0300

    Recuperando dados do servidor locarl

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -2,19 +2,23 @@ import './App.css';
 
 import { useState, useEffect } from "react";
 
+const url = "http://localhost:3000/products";
+
 function App() {
-  const [products, setProducts] = useState([])
+
+  const [products, setProducts] = useState([]);
 
   // 1 - resgatando dados
-  useEffect(async () => {
+  useEffect(async () => { //funções declaradas como async dizem para o js que uma PROMISE é esperada
 
-    const res = await fetch(url)
+    const res = await fetch(url) // fetch inicia o processo de buscca de um recurso da rede, retorna uma promessa que é cumprida assim que a resposta estiver disponivel
 
-    const data = await res.json()
+    const data = await res.json()// await é uma função js utilizada em conjunto com o async on o mesmo para a function até que a PROMISE seja cumprida.
 
-    setProducts(data)
+    setProducts(data);
+  }, []);
 
-  })
+  console.log(products);
 
   return (
     <div className="App">
