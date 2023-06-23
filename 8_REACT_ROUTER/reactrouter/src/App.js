0c640f685a5b85c5b60e31a0f7f4db58368ff965
Fell1p <luofelipe@hotmail.com>
2023-06-22 22:46:48 -0300

    Crando banco de dados, servidor e iniciando no react rauter

---
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+//const url = "http://localhost:3000/games";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+    </div>
+  );
+}
+
+export default App;
